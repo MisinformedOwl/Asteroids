8c6e1d96d5eab5158ec8484554ee2e47b72f6bb4
MisinformedOwl <60757710+MisinformedOwl@users.noreply.github.com>
2022-10-03 11:24:01 +0100

    Update memory.py

--- memory.py
+++ memory.py
@@ -14,7 +14,7 @@ class workingMemory:
     line8 = 0
 
 def Initialize():
-    rules = [] # 8³ * 9 = 59,049 rules
+    rules = [] # 3^8 * 9 = 59,049 rules
     
     dangers = ["low", "med", "high"]
     moves = [(1,0,0,0), (0,1,0,0), (0,0,1,0),(0,0,0,1), (0,0,0,0), (1,1,0,0), (1,0,1,0), (0,0,1,1), (0,1,0,1)] # (up, left, right, down)
